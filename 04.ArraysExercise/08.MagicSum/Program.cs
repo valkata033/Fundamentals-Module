f67e36a5b0452626ea319d21ae1b347e67578d3e
Valentin <85406438+valkata033@users.noreply.github.com>
2022-01-27 20:28:20 +0200

    Add files via upload

---
+++ 04.ArraysExercise/08.MagicSum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08.MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 04.ArraysExercise/09.KaminoFactory/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.KaminoFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            int maxSequence = 0;
+            int currentSequence = 1;
+            string element = String.Empty;
+            int mostLeftIndex = 0;
+
+            while (command != "Clone them!")
+            {
+                int[] numbers = command.Split("!").Select(int.Parse).ToArray();
+
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    if (numbers[i] == numbers[i + 1])
+                    {
+                        currentSequence++;
+                    }
+                    else
+                    {
+                        currentSequence = 1;
+                    }
+
+                    if (currentSequence > maxSequence)
+                    {
+                        maxSequence = currentSequence;
+                    }
+                }
+                command = Console.ReadLine();
+            }
+
+        }
+    }
+}
